597ca7a374d316880b8e25581eff05244b323e1a
Kesehet <hamood.siddiqui@gmail.com>
2023-09-16 01:53:45 +0530

    optimize

--- server.js
+++ server.js
@@ -11,6 +11,9 @@ const cors = require('cors');
 
 
 let senderStream = {};
+const STREAM_TIMEOUT = 5 * 60 * 1000; // 5 minutes
+let streamTimeouts = {};
+
 var cherry =[
     {
         urls: "turn:74.235.112.32:3478",
@@ -44,6 +47,16 @@ app.post("/consumer", async ({ body }, res) => {
 	if(streamNow == undefined){
 		res.json({})
 	}
+
+        // Reset the timeout for this stream since it's being accessed
+        if (streamTimeouts[body.connectionID]) {
+            clearTimeout(streamTimeouts[body.connectionID]);
+            streamTimeouts[body.connectionID] = setTimeout(() => {
+                delete senderStream[body.connectionID];
+                delete streamTimeouts[body.connectionID];
+                console.log(`Stream with connectionID ${body.connectionID} has been removed due to inactivity.`);
+            }, STREAM_TIMEOUT);
+        }
 	
     streamNow.getTracks().forEach(track => peer.addTrack(track, streamNow));
     const answer = await peer.createAnswer();
@@ -70,9 +83,21 @@ app.post('/broadcast', async ({ body }, res) => {
     res.json(payload);
 });
 
-function handleTrackEvent(e, peer,connID) {
+function handleTrackEvent(e, peer, connID) {
     senderStream[connID] = e.streams[0];
-};
+
+    // Clear any existing timeout for this stream
+    if (streamTimeouts[connID]) {
+        clearTimeout(streamTimeouts[connID]);
+    }
+
+    // Set a new timeout to remove the stream after the specified period
+    streamTimeouts[connID] = setTimeout(() => {
+        delete senderStream[connID];
+        delete streamTimeouts[connID];
+        console.log(`Stream with connectionID ${connID} has been removed due to inactivity.`);
+    }, STREAM_TIMEOUT);
+}
 
 var server = https.createServer(options, app);
 server.listen(443,'0.0.0.0', () => console.log('server started'));
