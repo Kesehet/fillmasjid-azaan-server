9a4545245489405e152a40b53d1a0fa55d80ed44
Kesehet <hamood.siddiqui@gmail.com>
2023-09-18 10:12:03 +0530

    rollback

--- server.js
+++ server.js
@@ -36,48 +36,45 @@ app.use(cors({origin: 'https://app.fillmasjid.in'}));
 
 var Broadcasts = {};
 
-class Broadcast {
-    constructor(connectionID) {
+
+class Broadcast{
+    constructor(connectionID){
         this.connectionID = connectionID;
         this.adminStream = null;
         this.consumerStreams = {};
     }
-
-    addAdminStream(stream) {
+    addAdminStream(stream){
         this.adminStream = stream;
     }
-
-    addConsumerStream(stream) {
-        if (this.consumerStreams[stream.version]) {
-            this.consumerStreams[stream.version].cleanup();
-        }
+    addConsumerStream(stream){
         stream.AttachTrackToListen(this.adminStream.track);
+        // if (this.consumerStreams[stream.version] != undefined) {
+        //     this.consumerStreams[stream.version].cleanup();
+        //     this.consumerStreams[stream.version] = null;
+        // }
         this.consumerStreams[stream.version] = stream;
     }
 
-    cleanupAllStreams() {
-        if (this.adminStream) {
-            this.adminStream.cleanup();
-        }
-        for (let version in this.consumerStreams) {
-            this.consumerStreams[version].cleanup();
-        }
-    }
 }
 
 
 class StreamObject {
-    constructor(connectionID, sdp, version, type = "consumer") {
+    constructor(connectionID, sdp, version,type ="consumer") {
         this.peer = new webrtc.RTCPeerConnection({ iceServers: cherry });
         this.desc = new webrtc.RTCSessionDescription(sdp);
         this.connectionID = connectionID;
         this.version = version;
-        this.type = type;
+        this.answer = null;
+        this.track = null;
+        this.type=type;
+        // Add event listeners
         this.addEventListeners();
+
+        // Set the remote description and wait for the correct state to load
         this.peer.setRemoteDescription(this.desc);
-        this.peer.ontrack = (e) => this.handleBroadcastStreamGetter(e);  // Reintroduced this line
+        this.peer.ontrack = (e) => this.handleBroadcastStreamGetter(e);
+
     }
-    
 
     addEventListeners() {
         this.peer.onicecandidate = (event) => {
@@ -102,23 +99,21 @@ class StreamObject {
 
         this.peer.onerror = (error) => {
             console.error('RTCPeerConnection error:', error);
-            this.cleanup();
         };
     }
 
-    async load() {
-        try {
-            const answer = await this.peer.createAnswer();
-            await this.peer.setLocalDescription(answer);
-        } catch (error) {
-            console.error("Error in load method:", error);
-        }
+async load() {
+    try {
+        const answer = await this.peer.createAnswer();
+        await this.peer.setLocalDescription(answer);
+    } catch (error) {
+        console.error("Error in load method:", error);
     }
-
+}
     cleanup() {
+        // Close the RTCPeerConnection
         this.peer.close();
-        if (this.type == "admin" && Broadcasts[this.connectionID]) {
-            Broadcasts[this.connectionID].cleanupAllStreams();
+        if(this.type=="admin"){
             delete Broadcasts[this.connectionID];
         }
         console.log('StreamObject resources released.');
@@ -149,11 +144,11 @@ class StreamObject {
 
 
 app.post("/consumer", async ({ body }, res) => {
-    if (!Broadcasts[body.connectionID]) {
-        res.json({});
-        return;
+    if(!Broadcasts[body.connectionID]){
+        res.json({})
+        return
     }
-    const stream = new StreamObject(body.connectionID, body.sdp, body.version);
+	const stream = new StreamObject(body.connectionID,body.sdp,body.version);
     Broadcasts[body.connectionID].addConsumerStream(stream);
     await stream.load();
     res.json(stream.response());
@@ -165,12 +160,17 @@ app.post("/consumer", async ({ body }, res) => {
 });
 
 app.post('/broadcast', async ({ body }, res) => {
-    const broadcast = new Broadcast(body.connectionID);
-    const stream = new StreamObject(body.connectionID, body.sdp, body.version, "admin");
+
+    const broadcast = new Broadcast(body.adminStream,body.connectionID);
+
+    const stream = new StreamObject(body.connectionID,body.sdp,body.version,type="admin");
     await stream.load();
     broadcast.addAdminStream(stream);
     Broadcasts[stream.connectionID] = broadcast;
     res.json(stream.response());
+
+	return;
+
 });
 
 app.get('/broadcast', async (req, res) => {
